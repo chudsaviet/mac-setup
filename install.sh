221ad2fae68e92a63107fbcc73b3605be8f64586
Chudsaviet <timofei_public@homeunix.by>
2018-12-28 15:24:43 -0800

    Initial work

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+brew install \
+	xz \
+	zstd \
+	unzip \
+	htop \
+	mc \
+	pv \
+	mtr \
+	micro \
+	bat \
+	httpie \
+	python2 \
+	python3 \
+	pyenv \
+	maven \
+	gradle \
+	awscli \
+	jq \
+	ffmpeg \
+	youtube-dl \
+	telnet \
+	pwgen \
+	git
+
+pip install --upgrade pip setuptools
+pip3 install --upgrade pip setuptools virtualenv
+
+brew cask install \
+	iterm2 \
+	firefox \
+	telegram \
+	skype \
+	spectacle \
+	intellij-idea-ce \
+	pycharm-ce \
+	visual-studio-code \
+	java \
+	docker \
+	vlc
