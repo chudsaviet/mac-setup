32a00cf53ef07b6c6aaab9420974de6e7d78238d
Timofei Korostelev <timofei_public@dranik.dev>
2026-03-16 16:03:11 -0700

    Added UI apps.

--- install.sh
+++ install.sh
@@ -11,9 +11,11 @@ brew tap homebrew-ffmpeg/ffmpeg
 echo "Installing commandline utilities..."
 brew install --force \
 	atomicparsley \
+	audacity \
 	automake \
 	awscli \
 	axel \
+	balenaetcher \
 	bat \
 	binutils \
 	bison \
@@ -23,7 +25,11 @@ brew install --force \
 	cmake \
 	coreutils \
 	doxygen \
+	epson-print-layout \
+	eqmac \
 	exiftool \
+	firefox \
+	foobar2000 \
 	gawk \
 	gcloud-cli \
 	git \
@@ -31,15 +37,19 @@ brew install --force \
 	glib \
 	gnu-sed \
 	go \
+	gyroflow \
 	htop \
 	httpie \
 	imagemagick \
 	iterm2 \
 	jq \
+	keepingyouawake \
+	kicad \
 	lsusb \
 	lzip \
 	m4b-tool \
 	make \
+	mas \
 	midnight-commander \
 	mosh \
 	mtr \
@@ -48,25 +58,33 @@ brew install --force \
 	nmap \
 	node \
 	ntp \
+	nvidia-nsight-systems \
 	ocrmypdf \
 	openjdk \
 	pigz \
 	pkgconf \
+	prusaslicer \
 	pv \
 	pwgen \
 	pyenv \
 	python \
+	rectangle \
 	rustup \
+	signal \
 	speedtest-cli \
+	steam \
 	telnet \
 	tmux \
 	transmission-cli \
 	unzip \
 	visual-studio-code \
+	vlc \
 	watch \
 	wget \
 	wireguard-tools \
+	wireshark-app \
 	yt-dlp
+
 
 echo "Building FFMPEG from tap..."
 brew install homebrew-ffmpeg/ffmpeg/ffmpeg --with-fdk-aac --with-libvmaf
@@ -79,3 +97,18 @@ source $HOME/.cargo/env
 
 echo "Installing Rust apps from Cargo..."
 cargo install jless
+
+echo "Installing apps from Mac App Store..."
+mas install 1451685025 # WireGuard UI
+mas install 747648890 # Telegram
+mas install 454347770 # iCircuit
+mas install 1381004916 # Discovery - DNS-SD Browser
+
+echo "Install next apps manually:"
+echo "1Password"
+echo "Autodesk Fusion 360"
+echo "DxO PhotoLab"
+echo "DxO FilmPack"
+echo "Filmlab"
+echo "REW (speaker design)"
+echo "LedgerLive"
